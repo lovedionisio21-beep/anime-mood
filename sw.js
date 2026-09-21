d3280b1a5d52c4590037d0e390f2175bd7624207
lovedionisio21-beep <lovedionisio21@gmail.com>
2026-09-21 15:54:01 -0400

    Prevent stale app caching during updates

--- sw.js
+++ sw.js
@@ -1,1 +1,32 @@
-const CACHE='anime-mood-v0.4';const ASSETS=['./','./index.html','./manifest.webmanifest'];self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==CACHE).map(x=>caches.delete(x))))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(c=>c||fetch(e.request).catch(()=>c))))
+const CACHE='anime-mood-v0.5';
+const ASSETS=['./','./index.html','./manifest.webmanifest'];
+
+self.addEventListener('install',event=>{
+  self.skipWaiting();
+  event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)));
+});
+
+self.addEventListener('activate',event=>{
+  event.waitUntil((async()=>{
+    const keys=await caches.keys();
+    await Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)));
+    await self.clients.claim();
+  })());
+});
+
+self.addEventListener('fetch',event=>{
+  if(event.request.method!=='GET') return;
+  event.respondWith((async()=>{
+    try{
+      const network=await fetch(event.request);
+      if(event.request.mode==='navigate'){
+        const cache=await caches.open(CACHE);
+        await cache.put('./index.html',network.clone());
+      }
+      return network;
+    }catch{
+      const cached=await caches.match(event.request);
+      return cached||caches.match('./index.html');
+    }
+  })());
+});
